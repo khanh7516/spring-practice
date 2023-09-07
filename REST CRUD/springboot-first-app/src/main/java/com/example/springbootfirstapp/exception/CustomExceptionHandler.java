1fddfadbc0e4cc5e17bbc4b5ad362c4973998bc0
KhanhNgo <62138525+khanh7516@users.noreply.github.com>
2023-09-07 10:36:18 +0700

    Summary

--- REST CRUD/springboot-first-app/src/main/java/com/example/springbootfirstapp/exception/CustomExceptionHandler.java
+++ REST CRUD/springboot-first-app/src/main/java/com/example/springbootfirstapp/exception/CustomExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.springbootfirstapp.exception;
 
 import com.example.springbootfirstapp.response.ErrorResponse;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
